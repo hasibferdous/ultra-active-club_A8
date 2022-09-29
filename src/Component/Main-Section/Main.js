88f304dd32b50cc36c5880e72c489377dcacb52f
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-29 23:20:15 +0600

    responsive website

--- src/Component/Main-Section/Main.js
+++ src/Component/Main-Section/Main.js
@@ -17,19 +17,7 @@ const Main = () => {
             console.log('local Storage first line', activities)
              const storedTime = getStoredTime();
              console.log(storedTime);
-        //     const savedTime = [];
-            // for(const id in storedTime){
-            //     const addedActivity = activities.find(activity=> activity.id === id);
-            //     console.log(addedActivity);
-                        
-            //     if(addedActivity){
-            //         const exerciseTime = storedTime[id];
-            //         addedActivity.exerciseTime =exerciseTime;
-            //         savedTime.push(addedActivity);
-            //      console.log(addedActivity);
-            //      }
-            // }
-            // setTime(savedTime);
+
     
         }, [activities])
     
@@ -37,26 +25,11 @@ const Main = () => {
             //console.log(activity);
             //time.push(activity);
             const newTime = [...time, activity];
-            // let newTime = [];
-            // const exists = time.find(activity => activity.id === selectedActivity.id);
-            // if(!exists){
-            //     selectedActivity.quantity = 1;
-            //     newTime = [...time, selectedActivity];
-            // }
-            // else {
-            //     const rest = time.filter(activity => activity.id !== selectedActivity.id);
-            //     exists.requiredTime = exists.requiredTime + 1;
-            //     newTime = [...rest, exists];
-            
-            // }
-            setTime(newTime);
+             setTime(newTime);
             addToDb(activity.id)
         }
 
-
-
-
-    return (
+        return (
         <div className='main-container'>
            <div className="activities-container">
             
